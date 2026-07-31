4443754924db8f68bd28b6219973781f1f397332
TK <tk>
2026-07-31 16:40:56 +0900

    Add sitemap and robots

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.cardcare.jp",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.cardcare.jp/commerce",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.cardcare.jp/privacy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.cardcare.jp/terms",
+      lastModified: new Date(),
+    },
+  ];
+}
