e5855631bc65e840493c056e47f767fe607569a4
TK <tk>
2026-07-31 22:47:54 +0900

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -52,7 +53,26 @@ export default function RootLayout({
       lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+  {children}
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-RM902PHCW3"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-tag" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-RM902PHCW3');
+    `}
+  </Script>
+
+</body>
     </html>
   );
 }
